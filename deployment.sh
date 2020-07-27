3229ddea4ae86539093ceea7be654675d9075e1f
gaberust <35743165+gaberust@users.noreply.github.com>
2020-07-27 02:24:18 -0400

    Update deployment.sh
    Hopefully this one works...

--- deployment.sh
+++ deployment.sh
@@ -1,40 +1,49 @@
-cd /var/www/foxden
+#!/usr/bin/bash
 
+# Update System
 sudo apt update
 sudo apt -y upgrade
 
+# Create Foxden System User
 sudo useradd --system foxden
+sudo mkdir -p /home/foxden
+sudo chown -R foxden:foxden /home/foxden
 
-sudo apt -y install gnupg
+# Install The Things
+sudo apt -y install gnupg build-essential ruby-full nginx git
 
+# Configure And Enable UFW
+sudo ufw allow OpenSSH
+sudo ufw allow "Nginx HTTP"
+sudo ufw enable
+
+# Install, Start, And Enable MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-
 sudo apt update
-
 sudo apt -y install mongodb-org
-
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
-sudo apt -y install build-essential ruby-full nginx
+# Download Source Code
+git clone https://github.com/gaberust/foxden.git
+sudo mkdir -p /var/www
+sudo mv foxden /var/www/foxden
+sudo mv /var/www/foxden/misc/nginx.conf /etc/nginx/nginx.conf
+sudo mv /var/www/foxden/misc/foxden-start /usr/bin/foxden-start
+sudo mv /var/www/foxden/misc/foxden-stop /usr/bin/foxden-stop
+sudo rmdir /var/www/misc
 
-sudo gem install bundler
+# Create Necessary Directory Structures
+sudo mkdir -p /var/www/foxden/tmp/sockets
+sudo mkdir -p /var/www/foxden/tmp/pids
+sudo mkdir -p /var/www/foxden/log
 
-sudo chown foxden:foxden /var/www/foxden
-sudo chmod 444 /var/www/foxden
-sudo mkdir -p /var/www/foxden/public/static/img/profile
-sudo chmod 644 /var/www/foxden/public/static/img/profile
+# Give Foxden User Ownership Of App Directory
+sudo chown -R foxden:foxden /var/www/foxden
 
-bundler install
+# Install Gem Dependencies
+sudo -u foxden bundle install --gemfile=/var/www/foxden/Gemfile
 
-sudo cp misc/nginx.conf /etc/nginx/nginx.conf
-
-sudo cp misc/foxden.service /etc/systemd/system/foxden.service
-sudo systemctl daemon-reload
-sudo systemctl start foxden
-sudo systemctl enable foxden
-
-sudo systemctl start nginx
-sudo systemctl enable nginx
+# Finished
+echo "Run foxden-start and foxden-stop to start and stop the application."
