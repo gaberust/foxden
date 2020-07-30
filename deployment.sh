211b8c568ebad55a7d81d494898077ad3498043b
gaberust <35743165+gaberust@users.noreply.github.com>
2020-07-29 22:44:36 -0400

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 
-# Check If Running As Root
-if [ $(id -u) = 0 ]; then
-    echo "BAD ROOT!"
-    exit 1
-fi
-
 # Download Source Code
 while [ ! -d "./foxden" ]
 do
@@ -13,54 +7,54 @@ do
 done
 
 # Update System
-sudo apt update
-sudo apt -y upgrade
+apt update
+apt -y upgrade
 
 # Create Foxden System User
-sudo useradd --system foxden
-sudo mkdir -p /home/foxden
-sudo chown -R foxden:foxden /home/foxden
+useradd --system foxden
+mkdir -p /home/foxden
+chown -R foxden:foxden /home/foxden
 
 # Install The Things
-sudo apt -y install gnupg build-essential ruby-full nginx git
+apt -y install gnupg build-essential ruby-full nginx git
 
 # Stop And Disable Nginx
-sudo systemctl disable nginx
-sudo systemctl stop nginx
+systemctl disable nginx
+systemctl stop nginx
 
 # Configure And Enable UFW
-sudo ufw allow OpenSSH
-sudo ufw allow "Nginx HTTP"
-sudo ufw enable
+ufw allow OpenSSH
+ufw allow "Nginx HTTP"
+ufw enable
 
 # Install, Start, And Enable MongoDB
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt update
-sudo apt -y install mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt update
+apt -y install mongodb-org
+systemctl start mongod
+systemctl enable mongod
 
 # Copy Source Code To Correct Destinations
-sudo mkdir -p /var/www
-sudo mv foxden /var/www/foxden
-sudo mv /var/www/foxden/misc/nginx.conf /etc/nginx/nginx.conf
-sudo mv /var/www/foxden/misc/foxden-start /usr/bin/foxden-start
-sudo chmod +x /usr/bin/foxden-start
-sudo mv /var/www/foxden/misc/foxden-stop /usr/bin/foxden-stop
-sudo chmod +x /usr/bin/foxden-stop
-sudo rmdir /var/www/foxden/misc
+mkdir -p /var/www
+mv foxden /var/www/foxden
+mv /var/www/foxden/misc/nginx.conf /etc/nginx/nginx.conf
+mv /var/www/foxden/misc/foxden-start /usr/bin/foxden-start
+chmod +x /usr/bin/foxden-start
+mv /var/www/foxden/misc/foxden-stop /usr/bin/foxden-stop
+chmod +x /usr/bin/foxden-stop
+rmdir /var/www/foxden/misc
 
 # Create Necessary Directory Structures
-sudo mkdir -p /var/www/foxden/tmp/sockets
-sudo mkdir -p /var/www/foxden/tmp/pids
-sudo mkdir -p /var/www/foxden/log
+mkdir -p /var/www/foxden/tmp/sockets
+mkdir -p /var/www/foxden/tmp/pids
+mkdir -p /var/www/foxden/log
 
 # Give Foxden User Ownership Of App Directory
-sudo chown -R foxden:foxden /var/www/foxden
+chown -R foxden:foxden /var/www/foxden
 
 # Install Gem Dependencies
-sudo gem install --no-document unicorn sinatra mongoid bcrypt json jwt
+gem install --no-document unicorn sinatra mongoid bcrypt json jwt
 
 # Finished
 echo "Run foxden-start and foxden-stop to start and stop the application."
